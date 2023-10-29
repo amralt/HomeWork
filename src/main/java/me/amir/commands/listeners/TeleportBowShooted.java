cf504a9bbb13b23982695cc4fb5d9b1f56be23c3
amralt <112940374+amralt@users.noreply.github.com>
2023-10-29 10:43:00 +0600

    Работает!

---
+++ src/main/java/me/amir/commands/listeners/TeleportBowShooted.java
@@ -0,0 +1,32 @@
+package me.amir.commands.listeners;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class TeleportBowShooted implements Listener {
+    @EventHandler
+    public void onShootEvent(ProjectileHitEvent event){
+
+        if (event.getEntity().getShooter() instanceof Player p){
+            ItemStack itemInMainHand = p.getInventory().getItemInMainHand();
+
+            if (Objects.equals(itemInMainHand.getItemMeta().displayName(), Component.text("teleport bow"))){
+                Location location = event.getEntity().getLocation();
+                p.teleport(location);
+                event.getEntity().remove();
+                p.sendMessage(Component.text(Color.green + "You was teleported" ));
+            }
+        }
+
+    }
+
+
+}
